99e3a626c7045e929c8a4c3afe74212cb6679092
Bungolian <nobuoariana@gmail.com>
2024-06-05 23:30:26 -0500

    Basic Button Functionality
    Generating Different Problems. Input Answer and reveal. Need to work on checking if the answer is correct. A big issue I run into so far is multiple displays. Might have to create a for loop. Might want to work on CSS now so I can get a better idea for future tricks.

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-
+// BACKGROUND FUNCTIONS 
 let x;
 let y;
 let answer;
-let result;
+let result = "Problem does not exist";
+let inputProblem;
 
 class multiplicationTricks{
 
     foiling2x2(){
         x = Math.floor(Math.random() * 99);
         y = Math.floor(Math.random() * 99);
-        let result = x * y;
-        return console.log(`${x} * ${y} = ${result}`);
+        result = x * y;
+        return `${x} * ${y}`;
     }   
 
     trick2by11(){
         x = Math.floor(Math.random() * 99);
         y = 11;
-        let result = x * y;
-        return console.log(`${x} * 11 = ${result}`); 
+        result = x * y;
+        return `${x} * ${y}`; 
     }
 
-    
     trick3by11 (){
         x = Math.floor(Math.random() * 999);
         y = 11;
-        let result = x * y;
-        return console.log(`${x} * 11 = ${result}`); 
+        result = x * y;
+        return `${x} * ${y}`; 
     }
 }
-
-
+//ADDING OBJECTS
 let mTricks = new multiplicationTricks;
 
-let document;
+// WEBPAGE FUNCTIONALITY
 const display = document.getElementById("display");
+const answerDisplay = document.getElementById("answer");
 
 function appendToDisplay(input){
     display.value += input;
@@ -41,8 +41,17 @@ function appendToDisplay(input){
 function clearDisplay(){
     display.value = "";
 }
-function generateProblem(){
-    display.value = "this is a new problem";
+function generateProblem(inputProblem){
+    display.value = inputProblem;
+}
+function reveal(){
+    answerDisplay.value = result;
+}
+function checkAnswer(input, inputProblem){ // not sure if this works... 
+    if (answer = result){
+        generateProblem(inputProblem);
+    }
+
 }
 
 
