0e448b257a0378bd7782194607af7a7397be473c
Bungolian <nobuoariana@gmail.com>
2024-06-11 21:47:08 -0500

    Check answer. Visual improvements.  Reading individual character.
    Check answer functional. However can't get it to check whenever there is user input. Nor can I keybind "enter" to the button. It seems to be a problem with the "addEventListener()".
    Added some visuals along with the check answer.
    Finally, I started some of the examples. I still have much to do. But first, I need to learn the syntax for arrays so I can store the individual character variables so I can use them later.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // BACKGROUND FUNCTIONS 
 let x;
 let y;
+let equation;
 let answer;
 let result = "Problem does not exist";
 let inputProblem;
+let str = "Placeholder String";
 
 class multiplicationTricks{
 //D for deriving the trick for examples. 
@@ -11,24 +13,28 @@ class multiplicationTricks{
         x = Math.floor(Math.random() * 99);
         y = Math.floor(Math.random() * 99);
         result = x * y;
-        return `${x} * ${y}`;
+        equation =`${x} * ${y}` 
+        return equation;
     }   
 
     trick2by11(){
         x = Math.floor(Math.random() * 99);
         y = 11;
         result = x * y;
-        return `${x} * ${y}`; 
+        equation = `${x} * ${y}`;
+        return equation; 
     }
-    Dtrick2by11(){
-        trick2by11()
+    Dtrick2by11(equation){
+        examples.characters(equation);
+        
     }
 
     trick3by11 (){
         x = Math.floor(Math.random() * 999);
         y = 11;
         result = x * y;
-        return `${x} * ${y}`; 
+        equation = `${x} * ${y}`;
+        return equation; 
     }
 }
 
@@ -36,36 +42,61 @@ class memorize {
     squaresBasic(){
         x = Math.floor(Math.random() * 30);
         result = x * x;
-        return `${x}^2`;
+        equation = `${x}^2`;
+        return equation;
+    }
+}
+class exampleVisuals{
+/*  TO DO
+Get each token in the variable. - several ways... I think about a lexer and parser...
+Create an array to store each character
+Come up with individual functions to represent dirivation. 
+Use array memories to print it out. 
+*/
+    characters(equation) { // A way of getting each character. 
+        for (let i = 0; i < equation.length; i++) {
+            const char = equation.charAt(i);
+            console.log(`Character at index ${i}: ${char}`);
+        }
     }
 }
 
 //ADDING OBJECTS
 let mTricks = new multiplicationTricks;
 let memTricks = new memorize;
+let examples = new exampleVisuals;
 
 // WEBPAGE FUNCTIONALITY
 const display = document.getElementById("display");
-const answerDisplay = document.getElementById("answer");
+const exampleDisplay = document.getElementById("exampleDisplay");
+let answerDisplay = document.getElementById("answer");
+let check = document.getElementById("check");
 
-function appendToDisplay(input){
-    display.value += input;
-}
+
 function clearDisplay(){
     display.value = "";
 }
 function generateProblem(inputProblem){
     display.value = inputProblem;
-    //display.value = "working";
+
+    answerDisplay.value = ""; 
+    answerDisplay.style.borderColor = "";
+}
+function generateExample(inputProblem){
+    exampleDisplay.value = inputProblem;
 }
 function reveal(){
-    answerDisplay.value = result;
+    answerDisplay.value = result;   
 }
-function checkAnswer(input, inputProblem){ // not sure if this works... 
-    if (answer = result){
-        generateProblem(inputProblem);
-    }
-
+function checkAnswer(input, inputProblem){
+        answer = answerDisplay.value;
+        if (answer == result){
+           console.log("Answer is correct");
+           answerDisplay.style.borderColor = "#57d945";
+           
+        }
+        else{
+            console.log("Incorrect");	
+            answerDisplay.style.borderColor = "#ff0000";
+        }
 }
-
-
