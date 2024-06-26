f5cd4bbdc6c30db3b828203b6bf8b9e6a2531c61
Bungolian <nobuoariana@gmail.com>
2024-06-26 01:57:02 -0500

    Steps kinda working
    Needs to make sure that the step cases break when the examples changes out of nowhere.  Also need to know more about arrays since its reading my chars as a string instead of an integer. Most likely a stoi from c++.

--- examples.js
+++ examples.js
@@ -1,41 +1,67 @@
-console.log ("Second Java file working"); // can pass parameters and variables between javascript files but not the actual funcitons. HTML acts as a middle man.
+let steps = [];
 
+let ones;
+let tens;
+let hundreds;
 
+let oneD;
+let tenD;
+let hundredD;
 
 class e2by11{   // abMMMyy . 01 * 56
-
+constructor(steps){
+    this.steps = new Array(4); // doesnt really do anthing.
+}
     readChar(equation) { 
-        char = []; 
-        for (let i = 0; i < equation.length; i++) {
-            char.push (equation.charAt(i));
+    char = []; 
+    for (let i = 0; i < equation.length; i++) {
+        char.push (equation.charAt(i));
+    }
+    ones = char[1];
+    tens = char[0];
+}
+    addStep(){
+        steps.push("step");
+        if (steps.length >= 5){
+            steps.length = 1;
+        }
+
+        switch (steps.length){
+            case 1:
+                console.log("case 1");
+                this.step1(equation);
+                nextStep.textContent = "Show Step 2";
+            break;
+            case 2:
+                console.log("case 2 ");
+                this.step2();
+                nextStep.textContent = "Show Step 3";
+            break;
+            case 3:
+                console.log("case 3 ");
+                this.step3();
+                nextStep.textContent = "Show Step 4";
+            break;
+            case 4:
+                console.log("case 4 ");
+                nextStep.textContent = "Show Step 5";
+            break;
         }
     }
 
-    step1(){
-        stepDisplay.value = char[1];
+    step1(equation){
+        this.readChar(equation);
+        oneD = ones;
+        stepDisplay.value = oneD;
         explainDisplay.textContent = "Ones digit remains the same.";
     }
     step2(){
-        stepDisplay.value = "Step Two";
-        explainDisplay.textContent = "Step Two";
+        stepDisplay.value = tens + ones;// adding like a string not a mathmatical equation
+        explainDisplay.textContent = "Add the 10's and the 1's place. Take the 1's digit of the sum and make it the 10's digit of your answer. Make sure to carry 1 over to the 100's digit in the case your sum is more than 9";
     }
     step3(){
         stepDisplay.value = "Step Three";
         explainDisplay.textContent = "Step Three";
-    }
-    explain(equation){
-        this.readChar(equation)
-        console.log(char); 
-        // -ROADBLOCK-
-        if (nextStep.textContent == "Show Step 1"){
-            nextStep.addEventListener("click", event =>{
-                this.step1();
-                nextStep.textContent = "Show Step 2";
-            })
-            
-        }
-
-        
     }
 }
 
@@ -46,7 +72,9 @@ function generateExample(inputProblem){
     exampleDisplay.value = inputProblem;
 }
 
-
-
+nextStep.addEventListener("click", event => {
+    ex2by11.addStep();
+    console.log(steps);
+});
 
 
