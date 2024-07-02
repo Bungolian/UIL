819d0c2df1a4dfbfb711f0b249dcdff23d6fe2f7
Bungolian <nobuoariana@gmail.com>
2024-07-01 19:21:44 -0500

    Foiling Done
    Added Carry function. Still need to use if statements because I need the scope?? Really wish I could've coded this with c++ so I could use pointers. I had to create an entire new variable just to store some info in when I could've just targeted the address :(
    I also believe that this method has a minor problem that occurs when you change problems mid-step. I've caught it 2-3 times, but It's really hard to pin point and identify.

--- examples.js
+++ examples.js
@@ -11,7 +11,19 @@ let hundredsB;
 let oneD;
 let tenD;
 let hundredD;
+
+let carryD; // the digit we will display on carries
+let carryAdd = 0; // the ammount that gets carried away into next function
 let Carry = false;
+
+
+function checkCarry(digit){ //only for 10 digits.
+        Carry = true;
+        carryD = `${digit}`.charAt(1);
+        carryAdd = `${digit}`.charAt(0);
+        return carryD , carryAdd;
+}
+
 
 function readChar(equation){// only works for 2x2 formulas's. In the future I wil simply create seprate readChars for more advanced formulas. Will most likely be in their own class if they arent currently shared by multiple tricks.
     char = []; 
@@ -63,17 +75,17 @@ class e2by11{   // abIIIyy . 01 * 56
     step2(){
         tenD = Number(ones) + Number(tens);
 
-        if (tenD >= 10){
-            Carry = true;
-            tenD = `${tenD}`;
-            tenD = tenD.charAt(1);
+        if(tenD >= 10){
+        checkCarry(tenD);
+        tenD = carryD;
         }
+        
         stepDisplay.value = tenD + oneD;
         explainDisplay.textContent = "Step 2 - Add the 10's and the 1's place. Take the 1's digit of the sum and make it the 10's digit of your answer. Make sure to carry 1 over to the 100's digit in the case your sum is more than 9";
     }
     step3(){
         hundredD = tens;
-        if (Carry == true){hundredD = `${Number(tens) + 1}`;}
+        if (Carry == true){hundredD = `${Number(tens) + Number(carryAdd)}`;}
         stepDisplay.value = hundredD + tenD + oneD;
         explainDisplay.textContent = "Step 3 - The Hundreds Digit of the final answer remains the same as the tens digit. UNLESS the sum from previous step requires a carry.";
         Carry = false;
@@ -112,13 +124,38 @@ class eFoiling2x2{ // abIIIcd . 01 *  56
     step1(equation){
         readChar(equation);
         oneD = Number(ones) * Number(onesB);
+        
+        if(oneD >= 10){ // check for a carry
+            checkCarry(oneD);
+            oneD = carryD;
+            //console.log(carryAdd);
+        }
+
         stepDisplay.value = oneD;
         explainDisplay.textContent = "Step 1 - Multiply the ones digits of both variables. Keep the ones digit of that answer and Carry if needed.";
     }
-    step2(){
+    step2(){// ten,one * tenB,oneB
+        tenD = Number(ones) * Number(tensB) + Number(tens) * Number(onesB);
+
+        if (Carry == true){ // check for previous carry
+            tenD = `${Number(tenD) + Number(carryAdd)}`;
+            Carry = false;
+            } 
+
+        if(tenD >= 10){
+            checkCarry(tenD);
+            tenD = carryD;
+        }
+        stepDisplay.value = tenD + oneD;
         explainDisplay.textContent = "Step 2 - Take the sum of the outer digits multiplied together and the inner digits multiplied together. (Outer-Inner = (ad + bc)) Keep the ones digit of that answer and Carry / Add previous Carries if needed.";  
     }
     step3(){
+        hundredD = Number(tens) * Number(tensB);
+        if (Carry == true){ // check for previous carry
+            hundredD = `${Number(hundredD) + Number(carryAdd)}`;
+            Carry = false;
+            }
+        stepDisplay.value = hundredD + tenD + oneD;
         explainDisplay.textContent = "Step 3 - Multiply the tens digits of both variables. Add previous Carries if needed.";
     }
     
